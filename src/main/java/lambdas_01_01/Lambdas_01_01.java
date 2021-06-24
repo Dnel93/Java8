a9472078d94c675999e66416688d10f2c53c39ad
Daniel Villarreal <dvillarreal93@gmail.com>
2021-06-23 17:12:57 -0700

    Added Method as lambdas

--- src/main/java/lambdas_01_01/Lambdas_01_01.java
+++ src/main/java/lambdas_01_01/Lambdas_01_01.java
@@ -1,4 +1,4 @@
-package functional_interfaces_01_01;
+package lambdas_01_01;
 
 import java.util.Locale;
 import java.util.function.*;
@@ -6,42 +6,42 @@ import java.util.function.*;
 public class Lambdas_01_01 {
 
     public static void main(String[] args) {
-        /**
+        /*
          * Predicate - Takes one argument returns a boolean
          *  Example: Using the test method of Predicate
          */
         Predicate<String> stringLen = s -> s.length() < 10;
         System.out.println(stringLen.test("Apples") + " - Apples is less than 10");
 
-        /**
+        /*
          * Consumer - Accepts single argument with no return value
          * Example: Consumer uses accept method
          */
         Consumer<String> consumerStr = s -> System.out.println(s.toLowerCase(Locale.ROOT));
         consumerStr.accept("ABCDefghijklmnopQRSTuvWxyZ");
 
-        /**
+        /*
          * Function - Accepts one argument and produces a result
          * Example: Function example
          */
         Function<Integer, String> converter = num -> Integer.toString(num);
         System.out.println("length of 26: " + converter.apply(26).length());
 
-        /**
+        /*
          * Supplier - Represents a supplier of results
          * Example: Function example
          */
         Supplier<String> s = () -> "Java is fun";
         System.out.println(s.get());
 
-        /**
+        /*
          * BinaryOperator - Takes two arguments and returns one
          * Example: Binary Operator Example
          */
         BinaryOperator<Integer> add = (a, b) -> a + b;
         System.out.println("Add 10 + 25: " + add.apply(10, 25));
 
-        /**
+        /*
          * UnaryOperator - Takes one argument with a return value
          * Example: Unary Operator Example
          */
---
+++ src/main/java/lambdas_01_03/Lambdas_01_03.java
@@ -0,0 +1,30 @@
+package lambdas_01_03;
+
+import java.math.BigInteger;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.IntFunction;
+import java.util.function.UnaryOperator;
+
+public class Lambdas_01_03 {
+    public static void main(String[] args) {
+
+        IntFunction<String> intToString = num -> Integer.toString(num);
+        System.out.println("expected value 3, actual value: " + intToString.apply(123).length());
+
+        // static method reference using ::
+        IntFunction<String> intToString2 = Integer::toString;
+        System.out.println("expected value 4, actual value: " + intToString2.apply(4567).length());
+
+        // lambdas made using a constructor
+        Function<String, BigInteger> newBigInt = BigInteger::new;
+        System.out.println("expected value: 123456789, actual value: " + newBigInt.apply("123456789"));
+
+        // Example of a lambda made from an instance method
+        Consumer<String> print = System.out::println;
+        print.accept("Coming to you directly from a lambda...");
+
+        UnaryOperator<String> makeGreeting = "Hello, "::concat;
+        System.out.println(makeGreeting.apply("Peggy"));
+    }
+}
