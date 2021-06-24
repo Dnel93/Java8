e89481a486553eb311aa6473f91aec1d5c64feba
Daniel Villarreal <dvillarreal93@gmail.com>
2021-06-23 18:28:11 -0700

    Added more examples of lambdas now with Streams

---
+++ src/main/java/lambdas_02_02/Lambdas_02_02.java
@@ -0,0 +1,36 @@
+package lambdas_02_02;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class Lambdas_02_02 {
+    public static void main(String[] args) {
+
+        Arrays.asList("red", "green", "blue")
+                .stream()
+                .sorted()
+                .findFirst()
+                .ifPresent(System.out::println);
+
+        /*
+            Example of Stream.of with a filter
+            ---------------------------------------------------
+            if the foreach is removed, nothing will print,
+            the foreach makes it a terminal event
+        */
+        Stream.of("apple","pear","banana","cherry","apricot")
+                .filter(fruit -> fruit.startsWith("a"))
+                .forEach(fruit -> System.out.println("Starts with A: " + fruit));
+
+        // using a stream and map operation to create a list of words in caps
+        List<String> collected = Stream.of("Java", "Rocks")
+                .map(String::toUpperCase)
+                .collect(toList());
+
+        System.out.println(collected);
+    }
+}
